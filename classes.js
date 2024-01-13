f4e03c34de697f63b5298f8430403d8b7625af02
mazharrasheed <mazharopal@gmail.com>
2024-01-13 16:59:22 +0500

    update classes and objects

--- classes.js
+++ classes.js
@@ -1,5 +1,4 @@
 
-
 let student={
     fullName:'Ayesha Mazhar',
     class:'five',
@@ -24,6 +23,87 @@ const naeem={
     sallary:20000,
 }
 
-faisal.__proto__=employe
-nadeem.__proto__=employe
-naeem.__proto__=employe
+faisal.__proto__=employe;
+nadeem.__proto__=employe;
+naeem.__proto__=employe ;
+
+class CarsToyota{
+
+    constructor(brand,model){
+        this.brand=brand;                     
+        this.model=model;
+    }
+    start(){
+        console.log('car can start');
+    }
+    stop(){
+        console.log('car can stop');
+    }
+
+    setBrand(brand){
+        this.brand=brand
+    }
+
+    setModel(model){
+        this.model=model
+    }
+}
+
+let corola=new CarsToyota('corola','2024'); // constructor invoke automaitcally on creation of an object.
+let yaris=new CarsToyota('yaris','2024');
+let fortuner=new CarsToyota('fortuner');
+
+const arr={class:"five",}
+
+
+class Parent{
+    heloo(){
+        console.log('hello');
+    }
+}
+
+class Chaild extends Parent {
+
+}
+
+let obj=new Chaild()
+
+class Person{
+
+    constructor(name){
+        this.name=name
+        this.species='home sapions'
+    }
+    eat(){
+        console.log('eat');
+    }
+    sleep(){
+        console.log('sleep');
+    }
+
+    work(){
+        
+        console.log('do nothing');
+    }
+}
+
+
+
+class Engineer extends Person {
+
+    constructor(branch,name){
+        console.log("enter child constructor");
+        super(name);
+        this.branch=branch;
+        console.log("exit child constructor");
+    }
+
+    work(){
+
+        super.eat();
+        console.log('solve problems');
+    }
+}
+
+
+let engrobj=new Engineer('Mechenical Engr','mazhar');
